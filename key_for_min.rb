47e340a64158690787b6a5c7204dedf806d86fd6
Amgad Malik <amgadmalik@yahoo.com>
2019-09-22 15:35:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,10 @@
 def key_for_min_value(name_hash)
     min_value = 0 
     min_key = ""
-  return nil if name_hash.nil?
+  if name_hash.nil?
+    return nil
+    break
+  end
   name_hash.each do |ke, val|
     min_value = val
     min_key = ke
