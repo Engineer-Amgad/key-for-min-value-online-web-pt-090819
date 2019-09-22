719996d25010cb316174bcec2f3ed598883b887f
Amgad Malik <amgadmalik@yahoo.com>
2019-09-22 16:02:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
     min_value = 0 
     min_key = ""
     min_key, min_value = name_hash.first 
-    binding
+    binding,pry
   if name_hash.empty?
     return nil
     #break
