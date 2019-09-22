d4097f9f6588456465a6b4e6446f1d7792e00d21
Amgad Malik <amgadmalik@yahoo.com>
2019-09-22 15:31:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |ke, val|
     min_value = val
     min_key = ke
-    if min_value > value
+    if min_value > val
       min_value = val
       min_key = ke
     end 
